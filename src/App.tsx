953ec242b5ba0cf3a76a9dc4326ba1de1bae5281
Aleksandr Grigorii <domosedov.dev@gmail.com>
2021-02-06 18:03:21 +0300

    work at audio player

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { createEvent, createStore } from "effector";
 import { useStore } from "effector-react";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { v4 as uuid } from "uuid";
 
 type Todo = {
@@ -33,15 +33,7 @@ $todos
   .on(createTodo, (todos, todo) => {
     return [...todos, todo];
   })
-  .on(removeTodo, (todos, id) => {
-    const index = todos.findIndex((todo) => todo.id === id);
-    if (index > -1) {
-      todos.splice(index, 1);
-      return [...todos];
-    } else {
-      return todos;
-    }
-  })
+  .on(removeTodo, (todos, id) => todos.filter((todo) => todo.id !== id))
   .on(toggleTodo, (todos, id) =>
     todos.map((todo) =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
@@ -52,11 +44,11 @@ function App() {
   const todoInputValue = useStore($todoInputValue);
   const todos = useStore($todos);
   const completedTodos = useStore($completedTodos);
-  const [audio] = useState(new Audio());
   const [audioSource, setAudioSource] = useState("/music.mp3");
-  const [play, setIsPlay] = useState(false);
-  const [playNext, setPlayNext] = useState(false);
-  const [sourceChanged, setSourceIsChanged] = useState(false);
+  const [audio] = useState(new Audio(audioSource));
+  const [audioIsPlaying, setAudioIsPlaying] = useState(false);
+  const [audioSourceIsChanged, setAudioSourceIsChanged] = useState(false);
+  const prevAudioSource = useRef(audioSource);
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
@@ -65,41 +57,64 @@ function App() {
   };
 
   useEffect(() => {
-    if (playNext) {
-      setSourceIsChanged(true);
-    }
-    audio.setAttribute("src", audioSource);
-  }, [audio, audioSource, playNext, setSourceIsChanged]);
+    audio.addEventListener("ended", () => setAudioIsPlaying(false));
+    return () =>
+      audio.removeEventListener("ended", () => setAudioIsPlaying(false));
+  }, [audio]);
 
   useEffect(() => {
-    if (play) {
+    if (audioIsPlaying) {
       audio.play();
     } else {
       audio.pause();
     }
-  }, [play, audio]);
+  }, [audioIsPlaying, audio]);
 
   useEffect(() => {
-    if (playNext && sourceChanged) {
-      setIsPlay(true);
+    if (audioSourceIsChanged) {
+      setAudioIsPlaying(true);
+      setAudioSourceIsChanged(false);
     }
-  }, [playNext, sourceChanged]);
+  }, [audioSourceIsChanged]);
+
+  useEffect(() => {
+    if (audioSource !== prevAudioSource.current) {
+      setAudioIsPlaying(false);
+      audio.setAttribute("src", audioSource);
+      setAudioSourceIsChanged(true);
+    }
+  }, [audio, audioSource]);
+
+  const handleAudioPlay = () => {
+    setAudioIsPlaying(true);
+  };
+
+  const handleAudioPaused = () => {
+    setAudioIsPlaying(false);
+  };
+
+  const handlePlayNext = () => {
+    prevAudioSource.current = audioSource;
+    setAudioSource("/music2.mp3");
+  };
+
+  const handlePlayPrev = () => {
+    let prev = audioSource;
+    setAudioSource(prevAudioSource.current);
+    prevAudioSource.current = prev;
+  };
 
   return (
     <div>
       <h1>Effector Sandbox</h1>
+
+      <h2>Audio is {audioIsPlaying ? "PLAY" : "PAUSED"}</h2>
+
       <h2>Current time</h2>
-      <button onClick={() => setIsPlay(true)}>PLAY</button>
-      <button onClick={() => setIsPlay(false)}>PAUSE</button>
-      <button
-        onClick={() => {
-          setIsPlay(false);
-          setAudioSource("/music2.mp3");
-          setPlayNext(true);
-        }}
-      >
-        NEXT
-      </button>
+      <button onClick={handlePlayPrev}>PREV</button>
+      <button onClick={handleAudioPlay}>PLAY</button>
+      <button onClick={handleAudioPaused}>PAUSE</button>
+      <button onClick={handlePlayNext}>NEXT</button>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
